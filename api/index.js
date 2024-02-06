0f2fed8dad587cc2a93ff05c32b39c0be1d9d7e7
shubhamgaurav26 <shubhgaurav26@gmail.com>
2024-02-07 05:29:26 +0530

    api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/user.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -21,7 +23,7 @@ app.listen(3000,() =>{
     console.log('Server is running on port 3000');
 });
 
-
+app.use('/api/user', userRoutes);
 
 
  
